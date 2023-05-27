5d241a080bb03b17084a76c11ba7775c9e6d6b6a
Mahbub <pabelmahbub@gmail.com>
2023-05-27 18:24:50 +0900

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,35 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import  axios from 'axios';
 
 export default createStore({
   state: {
+    counter: 0
   },
   getters: {
+    counterSquared: state => {
+      return state.counter * state.counter;
   },
+},
   mutations: {
+    DECREMENT_COUNTER(state, randomNumber){
+      console.log(randomNumber);
+      state.counter -= randomNumber;
+    },
+    INCREMENT_COUNTER(state, randomNumber) {
+      console.log(randomNumber);
+      state.counter += randomNumber;
+     }
   },
   actions: {
+    DECREMENT_COUNTER({commit}){
+      axios("https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new").then(response => commit('DECREMENT_COUNTER',response.data))
+      
+    },
+    INCREMENT_COUNTER({commit}){
+      axios("https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new").then(response => commit('INCREMENT_COUNTER',response.data))
+      
+    },
   },
   modules: {
   }
-})
+})       
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="counter">VUEX LEARNING</p>
+    <p class="counter">Counter: {{ store.state.counter }}</p>
+    <P>{{ store.state.counter }}<sup>2</sup>= {{ store.getters.counterSquared }}</P>
+
+    <div class="buttons">
+      <button @click="store.dispatch('DECREMENT_COUNTER')">-</button>
+      <button @click="store.dispatch('INCREMENT_COUNTER')">+</button>
+   </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import store from '../store';
+import { ref } from 'vue';
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  const count = ref(0); // Define a reactive variable using ref()
+
+  
 </script>
+
+
+<style scoped>
+.counter{
+  font-size: 40px;
+  font-weight: 700;
+}
+.buttons button {
+  width:120px;
+  margin:30px 10px;
+  font-size:40px
+  }
+
+</style>
